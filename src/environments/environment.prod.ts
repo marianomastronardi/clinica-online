81b68535ed8da4931262417f4fdac677b65b64ec
Your Name <you@example.com>
2021-05-15 12:39:22 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCneGTd4a8ZvGvP4toZu3iNaUFD6EosyNk",
+    authDomain: "clinica-online-108050.firebaseapp.com",
+    databaseURL: "https://clinica-online-108050-default-rtdb.firebaseio.com",
+    projectId: "clinica-online-108050",
+    storageBucket: "clinica-online-108050.appspot.com",
+    messagingSenderId: "952860761187",
+    appId: "1:952860761187:web:7dca13498d4f3ea304ac56"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCneGTd4a8ZvGvP4toZu3iNaUFD6EosyNk",
+    authDomain: "clinica-online-108050.firebaseapp.com",
+    databaseURL: "https://clinica-online-108050-default-rtdb.firebaseio.com",
+    projectId: "clinica-online-108050",
+    storageBucket: "clinica-online-108050.appspot.com",
+    messagingSenderId: "952860761187",
+    appId: "1:952860761187:web:7dca13498d4f3ea304ac56"
+  }
 };
 
 /*
